bb8b71c1a7e703af75e39c8e829051984b953e19
CesarAdan1 <whoclash@hotmail.com>
2019-04-25 18:14:07 -0500

    Initial Commit

---
+++ C++ Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Practice
+//
+//  Created by Cesar  on 4/25/19.
+//  Copyright © 2019 Cesar . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
